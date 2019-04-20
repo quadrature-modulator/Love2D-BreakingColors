136c07b8fe502fa9359f2edeae6c621659da4f29
Joseph <jgarcia180@sweetwaterschools.net>
2019-04-20 08:40:17 -0700

    Update Gravity.lua

--- Gravity.lua
+++ Gravity.lua
@@ -1,5 +1,12 @@
 Gravity={}
+local tilemap = require 'tilemap'
+funtion loop()
+if sameCounter > 2 then
+    for k,v in pairs(tempDel.x) do del.x[#del.x + k] = v end
+    for k,v in pairs(tempDel.y) do del.y[#del.y + k] = v end
+ 
+end
 funtion Gravity.fall()
-    
+     
 end
 return Gravity
